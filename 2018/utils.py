034236bbe7e51aa0d3b6383a89cbe60ece467800
Brenda So <sogun3@gmail.com>
2018-04-25 23:30:20 -0400

    Merged Gitzels new code

--- 2018/utils.py
+++ 2018/utils.py
@@ -1,17 +1,18 @@
-from segGenTest import TCPsegment, TCPsegmentDecode
+import datetime
+import logging
 
 
-# make a TCP checksum for the checksum field
-# due to the nature of TCPsegment class, this function should be called
-# after segment.Pack() is called
-# during checksum calculation, the checksum field itself is filled with zeros
-# @param tcp_seg: A TCPsegment class
-# returns a 16bit checksum string
-def make_checksum(tcp_seg):
-    checksum_string = "0000000000000000"    
-    tcp_bitstring = tcp_seg.tcp_seg_bitstr
-    return checksum_string
+class Logger(object):
 
-# Check TCP checksum to make sure the checksum field is correct
-def check_checksum(tcp_seg):
+    def __init__(self, name, debug_level):
+        now = datetime.datetime.now()
+        logging.basicConfig(filename='{}_{}.log'.format(name, datetime.datetime.strftime(now, "%Y_%m_%dT%H%M%S")),
+                            level=debug_level)
 
+    @staticmethod
+    def info(message):
+        logging.info(message)
+
+    @staticmethod
+    def debug(message):
+        logging.debug(message)
