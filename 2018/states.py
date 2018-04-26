f2673a0c5f105a90f169fcbf7ca0670fbe30a85d
Brenda So <sogun3@gmail.com>
2018-04-26 14:35:47 -0400

    Git removed states.py

--- 2018/states.py
+++
@@ -1,8 +0,0 @@
-# Represent States as integers, add more states if necessary
-
-IDLE = 0            # Sender is waiting for info from application layer
-LISTEN = 1          # Receiver listening for new data in the channel
-SYN_SEND = 2        # Sender sent SYN packet to receiver, waiting for response
-SYN_RECEIVED = 3    # Receiver received the SYN packet, sends SYNACK packet back
-ESTABLISHED = 4     # Sender and Receiver establishes the connection
-FIN_WAIT_1 = 5      # Sender and Receiver waiting to finish the connection
